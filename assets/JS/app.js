f501e918fd4cb362d58efc6b835a80eabd86d5b1
BransonJ <joshua.branson@lrs.com>
2023-03-21 13:48:42 -0500

    all calls are in place

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -12,3 +12,21 @@ fetch('https://opencritic-api.p.rapidapi.com/game/13349', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
+
+// Halo 5 call
+fetch('https://opencritic-api.p.rapidapi.com/game/1513', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+// Company of Heroes 3 call
+fetch('https://opencritic-api.p.rapidapi.com/game/14319', options)
+.then(response => response.json())
+.then(response => console.log(response))
+.catch(err => console.error(err));
+
+// Metroid Prime remastered
+fetch('https://opencritic-api.p.rapidapi.com/game/14280', options)
+.then(response => response.json())
+.then(response => console.log(response))
+.catch(err => console.error(err));
